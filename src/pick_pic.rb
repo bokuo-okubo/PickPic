2addc03d28372714e5532d0a633d725846257749
Yohei Okubo <okubo.yohei@gmail.com>
2015-10-24 01:35:51 +0900

    hoge

--- src/pick_pic.rb
+++ src/pick_pic.rb
@@ -1,6 +1,9 @@
 require 'yaml'
 require 'fileutils'
 require 'thwait'
+require 'net/http'
+require 'uri'
+require 'logger'
 
 BASE_PATH = './data_source/'
 
@@ -17,68 +20,97 @@ class << Thread
     Thread.original_new(args,&block)
   end
 
-  def Thread.max_concurrent=(num = 10)
-    Thread.main[:max_concurrent] = num
+  def Thread.max_concurrent=(num)
+    Thread.main[:max_concurrent]=num
+  end
+end
+
+
+module Filer
+  def get_file_path(message)
+    puts message
+    gets.to_s.chomp
+  end
+
+  def write_file(str, file_path, mode: 'w')
+    begin
+      File.open(file_path, mode) { |f| f.write str}
+    rescue => e
+      puts e
+    end
+  end
+
+  def file_open_as_array(file_name)
+    list = []
+    begin
+      File::open(BASE_PATH + file_name) do |f|
+        f.each { |line| list << line.to_s.chomp }
+      end
+    rescue => e
+      p e
+      file_open_as_array( get_file_path 'file_name is wrong. try again:' )
+    end
+    list
+  end
+end
+
+class Logger
+  @logger
+  def self.log(err)
+    self.create.fatal(err)
+  end
+  private
+  def self.create
+    @logger ||= Logger.new('error.log')
   end
 end
 
 module PickPic
+  extend Filer
+
   class << self
     def run
       message = 'Type the filename of URL-list:'
       puts url_list = file_open_as_array( get_file_path message )
-
-      message = <<-"EOS"
-----------------------------------------
-
-Type the directory name of output data:
-EOS
-
+      puts '----------------------------------------'
+      puts "url count is: #{url_list.length}"
+      message ="Type the directory name of output data:"
       save_path = get_file_path(message) + "/"
 
       FileUtils.mkdir_p save_path unless FileTest.exist? save_path
 
       threads = []
-      url_list.each do |url|
-        threads << Thread.new { Fetch.curl(url, save_path) }
-      end
       puts 'work in multi-thread'
+      url_list.each do |url|
+        Thread.max_concurrent = 10
+        threads << Thread.new do |th|
+          response = Fetch.curl(url)
+          url.split("/").last
+          filename = url.split("/").last
+          begin
+            write_file(response.body, save_path + filename)
+          rescue => e
+            Logger.log(e)
+          end
+        end
+      end
       ThreadsWait.all_waits(*threads) { |th| printf "." }
       puts "\ndone!"
-    end
-
-    def get_file_path(message)
-      puts message
-      gets.to_s.chomp
-    end
-
-    def file_open_as_array(file_name)
-      list = []
-      begin
-        File::open(BASE_PATH + file_name) do |f|
-          f.each { |line| list << line.to_s.chomp }
-        end
-      rescue => e
-        p e
-        file_open_as_array( get_file_path 'file_name is wrong. try again:' )
-      end
-      list
     end
   end
 
   module Fetch
-
-    def self.curl(uri, save_path)
-      ary = uri.to_s.split("/")
-      save_file_name = ary[-2].to_s + "-" + ary[-1].to_s
+    def self.curl(uri, save_path = nil)
+      uri = URI.parse(uri)
+      http = Net::HTTP.new(uri.host, uri.port)
       begin
-        `curl --anyauth --user 4d:4d #{ uri } -o #{ save_path + save_file_name } >/dev/null 2>&1`
+        request = Net::HTTP::Get.new(uri.request_uri)
       rescue => e
-        puts e
-        File.open("../error.log", "a") { |f| f.write e }
+        Logger.log(e)
       end
+      request.basic_auth('4d', '4d')
+      response = http.request(request)
     end
   end
 end
-
 PickPic.run
